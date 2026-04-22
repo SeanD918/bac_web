2f563c76d9eabe1d0709a17fe8cc9de34f8912bd
Your Nameiheb <your@email.comrouiniiheb@fstsbz.u-kairouan.tn>
2026-04-22 23:18:02 +0200

    Add initialization error catch in api/index.js to debug 500 errors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 console.log('[Vercel] API Entry point loaded');
 // This is the Vercel serverless entry point
 // It wraps your Express app and makes it work on Vercel
-const app = require('../server/index');
-module.exports = app;
+try {
+  const app = require('../server/index');
+  module.exports = app;
+} catch (err) {
+  module.exports = (req, res) => {
+    res.status(500).json({ 
+      error: 'Failed to load server/index', 
+      message: err.message, 
+      stack: err.stack 
+    });
+  };
+}
